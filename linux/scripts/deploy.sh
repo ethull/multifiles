27ba507242c26d8d40b0faba52b07776c996ed4f
Eth-H <ethull@protonmail.com>
2023-11-17 23:45:42 +0000

    update linux refresh script

--- linux/scripts/deploy.sh
+++ linux/scripts/deploy.sh
@@ -1,25 +1,40 @@
 # deploy dotfiles
 
+create_dir () {
+  TARGET_PATH=$1
+  [ ! -d $TARGET_PATH ] && mkdir $TARGET_PATH
+}
+
+refresh_dir () {
+  TARGET_PATH=$1
+  [ -d $TARGET_PATH ] && rm -r $TARGET_PATH
+}
+
+refresh_file () {
+  TARGET_PATH=$1
+[ -f $TARGET_PATH ] && rm -r $TARGET_PATH
+}
+
 cd ..
 CWD=$(pwd)
 
 ## create config directories if they dont exist
-[ ! -d $HOME/.config ] && mkdir $HOME/.config
+create_dir $HOME/.config
 ### linux config dirs only
-[ ! -d $HOME/.local ] && mkdir $HOME/.local
-[ ! -d $HOME/.local/bin ] && mkdir $HOME/.local/bin
-[ ! -d $HOME/.local/share ] && mkdir $HOME/.local/share
-#[ -d $HOME/.local/share/applications ] && rm -r $HOME/.local/share/applications
+create_dir $HOME/.local
+create_dir $HOME/.local/bin
+create_dir $HOME/.local/share
+#refresh_dir $HOME/.local/share/applications
 #ln -sf $CWD/linux/.local/share/applications $HOME/.local/share/applications
 
 ## setup program configs
 
 ### vim
-[ -d $HOME/.vim ] && rm -r $HOME/.vim
+refresh_dir $HOME/.vim
 ln -sf $CWD/unix/.vim $HOME/.vim
 
 ### nvim
-[ -d $HOME/.config/nvim ] && rm -r $HOME/.config/nvim
+refresh_dir $HOME/.config/nvim
 ln -sf $CWD/unix/.config/nvim $HOME/.config/nvim
 
 ### git
@@ -27,25 +42,25 @@ ln -sf $CWD/.gitignore_global $HOME/.gitignore_global
 
 ### tmux
 #tmux only reads $HOME/.tmux.conf
-[ -f $HOME/.tmux.conf ] && rm -r $HOME/.tmux.conf
-[ -d $HOME/.config/tmux ] && rm -r $HOME/.config/tmux
+refresh_file $HOME/.tmux.conf
+refresh_dir $HOME/.config/tmux
 ln -sf $CWD/unix/.config/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $CWD/unix/.config/tmux $HOME/.config/tmux
 
 ### mocp
-[ -d $HOME/.moc ] && rm -r $HOME/.moc
+refresh_dir $HOME/.moc
 ln -sf $CWD/unix/.moc $HOME/.moc
 
 ### eclipse vrapper
-#[ -d $HOME/.vrapperrc ] && rm -r $HOME/.vrapperrc
+#refresh_file $HOME/.vrapperrc
 #ln -sf $CWD/unix/.vrapperrc $HOME/.vrapperrc
 
 ### vifm (linux config)
-[ -d $HOME/.config/vifm ] && rm -r $HOME/.config/vifm
+refresh_dir $HOME/.config/vifm
 ln -sf $CWD/linux/.config/vifm $HOME/.config/vifm
 
 ### zathura
-#[ -d $HOME/.config/zathura ] && rm -r $HOME/.config/zathura
+refresh_dir $HOME/.config/zathura
 #ln -sf $CWD/linux/.config/zathura $HOME/.config/zathura
 
 ## scripts // just symlink dir?
@@ -59,15 +74,15 @@ ln -sf $CWD/linux/.local/bin/flac-to-mp3-rm-recursive.sh $HOME/.local/bin/flac-t
 
 ## xserver and shell configs
 #ln -sf $CWD/.bashrc $HOME/.bashrc
-[ -f $HOME/.bash_aliases ] && rm -r $HOME/.bash_aliases
+refresh_file $HOME/.bash_aliases
 ln -sf $CWD/linux/.bash_aliases $HOME/.bash_aliases
-[ -f $HOME/.profile ] && rm -r $HOME/.profile
+refresh_file $HOME/.profile
 ln -sf $CWD/linux/.profile $HOME/.profile
-[ -f $HOME/.xresources ] && rm -r $HOME/.xresources
+refresh_file $HOME/.xresources
 ln -sf $CWD/linux/.xresources $HOME/.xresources
-[ -f $HOME/.xprofile ] && rm -r $HOME/.xprofile
+refresh_file $HOME/.xprofile
 ln -sf $CWD/linux/.xprofile $HOME/.xprofile
-[ -f $HOME/.xinitrc ] && rm -r $HOME/.xinitrc
+refresh_file $HOME/.xinitrc
 ln -sf $CWD/linux/.xinitrc $HOME/.xinitrc
 
 ## grub config
@@ -83,9 +98,9 @@ sudo cp -r $CWD/linux/assets/themes/Numix /usr/share/themes/Numix
 sudo cp -r $CWD/linux/assets/themes/Midnight-OrangeNight /usr/share/themes/Midnight-OrangeNight
 
 ## xfce displays, keybindings, power settings
-[ -f $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml ] && rm -r $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml
+refresh_file $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml
 ln -sf $CWD/linux/.config/xfce4/xfce-perchannel-xml/displays.xml $HOME/.config/xfce4/xfce-perchannel-xml/displays.xml
-[ -f $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ] && rm -r $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+refresh_file $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 ln -sf $CWD/linux/.config/xfce4/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml $HOME/.config/xfce4/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
-[ -f $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml ] && rm -r $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+refresh_file $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
 ln -sf $CWD/linux/.config/xfce4/xfce-perchannel-xml/xfce4-power-manager.xml $HOME/.config/xfce4/xfce-perchannel-xml/xfce4-power-manager.xml
